5fbcedd4149f14a102bd12ebc4e1dc8fa38381df
jinosh <jinosh.mindorigin@gmail.com>
2023-07-19 17:57:25 +0530

    constants added

---
+++ lib/constants/strings.dart
@@ -0,0 +1,13 @@
+part of 'resources.dart';
+
+///
+/// [S] Stands for AppStrings
+///
+class S {
+  static const String hello = "Hello";
+
+  // static const String a = "";
+}
+
+  // static const String a = "";
+
