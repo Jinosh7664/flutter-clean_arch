540565e414cabdf679fb0410957a46ba59c485da
jinosh <jinosh.mindorigin@gmail.com>
2023-07-20 11:24:57 +0530

    Flutter Bloc added

--- lib/configs/apptheme.dart
+++ lib/configs/apptheme.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData theme = ThemeData.light(useMaterial3: true).copyWith(
+  static ThemeData lighttheme = ThemeData.light(useMaterial3: true).copyWith(
     scaffoldBackgroundColor: Colors.white,
     appBarTheme: const AppBarTheme(
       backgroundColor: Colors.white,
     ),
   );
+  static ThemeData darktheme = ThemeData.light(useMaterial3: true).copyWith();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,54 @@
+import 'package:clean_architecture/configs/app.dart';
+import 'package:clean_architecture/configs/apptheme.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // return MultiBlocProvider(
+    //   providers: const [],
+    //   child: const MainApp(),
+    // );
+    return const MainApp();
+  }
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      navigatorKey: navigatorKey,
+      themeMode: ThemeMode.dark,
+      theme: AppTheme.lighttheme,
+      darkTheme: AppTheme.darktheme,
+      debugShowCheckedModeBanner: false,
+      builder: (context, child) {
+        ///
+        /// Initial Properties of app are initiated
+        ///
+        App.init(context);
+        return child!;
+      },
+      home: FutureBuilder(
+        builder: (context, snapshot) {
+          return const Scaffold();
+        },
       ),
     );
   }
